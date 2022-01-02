72beac2700d23e99a6b9cc592639e4fbf94d7275
kiyeon-k <kiyeon.k@linecorp.com>
2022-01-02 23:42:35 +0900

    [20220102] Initial commit

---
+++ src/test/java/com/kiyeon/practice/springgraphqlpractice/SpringGraphqlPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiyeon.practice.springgraphqlpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGraphqlPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
